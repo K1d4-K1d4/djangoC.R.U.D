ad1f8d201da49addfd7dfbe8216336e6b359f5ff
Pedro Henrique de Macedo Oliveira <pedrokida@gmail.com>
2025-09-17 19:58:20 -0300

    :sparkles: Feat: Criação de modelo Livro | Criação de serializer para modelo Livro

--- appCRUD/models.py
+++ appCRUD/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class Livro(models.Model):
+#  ID do livro no Google Books (para evitar duplicatas)
+   google_books_id = models.CharField(max_length=100, unique=True)
+#  Campos do livro
+   titulo = models.CharField(max_length=255)
+   autor = models.CharField(max_length=255)
+   capa_url = models.URLField(max_length=500, blank=True, null=True)
+#  Campo que o usuário pode preencher
+   nota = models.IntegerField(
+      validators=[MinValueValidator(1), MaxValueValidator(10)]
+   )
+   resenha = models.TextField(blank=True, null=True)
+
+
+   def __str__(self):
+      return self.titulo
--- appCRUD/serializer.py
+++ appCRUD/serializer.py
@@ -1,1 +1,16 @@
 from rest_framework import serializers
+from .models import Livro
+
+class LivroSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Livro
+      fields = [
+         'id', 
+         'google_books_id', 
+         'titulo', 
+         'autor', 
+         'capa_url', 
+         'nota', 
+         'resenha'
+      ]
+      read_only_fields = ['id', 'google_books_id', 'autor', 'capa_url']
